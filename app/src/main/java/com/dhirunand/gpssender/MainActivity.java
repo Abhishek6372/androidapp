5b4525157a025631c640d282effe6600df3cc2d0
Dhiraj Kumar <dhirajyadav18062000@gmail.com>
2022-04-03 22:33:43 +0530

    all working 03.04.2022

--- app/src/main/java/com/dhirunand/gpssender/MainActivity.java
+++ app/src/main/java/com/dhirunand/gpssender/MainActivity.java
@@ -1,28 +1,59 @@
 package com.dhirunand.gpssender;
 
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.annotations.Nullable;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private final int REQUEST_LOCATION_PERMISSION = 1;
 
@@ -33,6 +64,11 @@ public class MainActivity extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
 
+    private GoogleMap mMap;
+    MarkerOptions marker;
+
+
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -59,30 +95,94 @@ public class MainActivity extends AppCompatActivity {
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("locations").child("dhiruPhone");
 
-        databaseReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                locationTV.setText(snapshot.toString());
-            }
+//        databaseReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                locationTV.setText(snapshot.toString());
+//
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
-            }
-        });
+
+        db = FirebaseFirestore.getInstance();
+//        fetchDataFromFirestore();
+
+//        new CountDownTimer(300000000, 1000) {
+//
+//            public void onTick(long millisUntilFinished) {
+////                mTextField.setText("seconds remaining: " + millisUntilFinished / 1000);
+//                //here you can have your logic to set text to edittext
+////                fetchDataFromFirestore();
+//            }
+//
+//            public void onFinish() {
+////                mTextField.setText("done!");
+//            }
+//
+//        }.start();
+
+
     }
 
     private final LocationListener mLocationListener = new LocationListener() {
         @Override
         public void onLocationChanged(final Location location) {
             //your code here
-            locationTV.setText(location.toString());
+            locationTV.setText("Latutude:"+location.getLatitude()+ " Longitude:"+location.getLongitude()+ " speed:"+location.getSpeed()+"m/s");
+//            locationTV.setText(location.toString());
 
-            databaseReference.setValue("latitude "+location.getLatitude()+"   longitude "+location.getLongitude());
+
+            LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+
+//            marker = new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title("Bus location");
+//            marker.icon(bitmapDescriptorFromVector(MainActivity.this, R.drawable.ic_baseline_directions_bus_24));
+//            mMap.addMarker(marker);
+
+
+            mMap.clear();
+            mMap.addMarker(new MarkerOptions()
+                    .position(sydney)
+                    .icon(bitmapDescriptorFromVector(MainActivity.this, R.drawable.ic_baseline_directions_bus_24))
+                    .title(location.getSpeed()+"m/s"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 15));
+
+//            databaseReference.setValue("latitude " + location.getLatitude() + "   longitude " + location.getLongitude());
+
+            HashMap<String, Object> hm = new HashMap<>();
+            hm.put("driver_id", "WB025601");
+            hm.put("latitude", location.getLatitude());
+            hm.put("longitude", location.getLongitude());
+            hm.put("vehicle_number", "WB0256");
+            hm.put("vehicle_type", "BUS");
+
+            db.collection("Vehicle_Details")
+                    .document("WB0256")
+                    .set(hm)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.v("TAGDhiruUpdate", e.toString());
+                }
+            });
+
+
 
         }
     };
-
 
 
     @Override
@@ -96,13 +196,59 @@ public class MainActivity extends AppCompatActivity {
     @AfterPermissionGranted(REQUEST_LOCATION_PERMISSION)
     public void requestLocationPermission() {
         String[] perms = {Manifest.permission.ACCESS_FINE_LOCATION};
-        if(EasyPermissions.hasPermissions(this, perms)) {
+        if (EasyPermissions.hasPermissions(this, perms)) {
             Toast.makeText(this, "Permission already granted", Toast.LENGTH_SHORT).show();
 
 
-        }
-        else {
+        } else {
             EasyPermissions.requestPermissions(this, "Please grant the location permission", REQUEST_LOCATION_PERMISSION, perms);
         }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Dhiru test Marker"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+
+    private void fetchDataFromFirestore() {
+        db.collection("Vehicle_Details")//WB0256
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d("TAGDhiru", document.getId() + " => " + document.getData());
+                                locationTV.setText(document.getData().toString());
+                            }
+                        } else {
+                            Log.w("TAGDhiru", "Error getting documents.", task.getException());
+                        }
+                    }
+                });
     }
 }
